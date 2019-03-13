e94874220810039c7c3cbee47033c002b6c95728
Erika Griechisch <griechisch.erika@gmail.com>
2019-03-13 08:37:19 +0100

    UPDATE arrangment

--- scriptek/bash/case.sh
+++ scriptek/bash/case.sh
@@ -3,22 +3,22 @@
 # Forras: Buki Andras: Hejprogramozas
 # Van-e parancssori parameter
 if [ $# -eq 0 ]; then
-	echo "Adjon meg parancssori paraméterként egy egyjegyű számot!"
+	echo "Adjon meg parancssori parameterkent egy egyjegyu szamot!"
 	exit 1
 fi
 
-echo -n "Ön a következő számot adta meg: "
+echo -n "On a kovetkezo szamot adta meg: "
 case $1 in
 	1) echo "Egy";;
-	2) echo "Kettő" ;;
-	3) echo "Három" ;;
-	4) echo "Négy" ;;
-	5) echo "Öt" ;;
+	2) echo "Ketto" ;;
+	3) echo "Harom" ;;
+	4) echo "Negy" ;;
+	5) echo "Ot" ;;
 	6) echo "Hat" ;;
-	7) echo "Hét" ;;
+	7) echo "Het" ;;
 	8) echo "Nyolc" ;; 
 	9) echo "Kilenc" ;; 
-	*) echo "Ez nem egyjegyű szám"; exit 2;;
+	*) echo "Ez nem egyjegyu szam"; exit 2;;
 esac
 
 exit 0
